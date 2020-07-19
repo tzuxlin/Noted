622b3a9f69ffe91acda7beafdefbbe28a602e035
Connie Lin <annul7tin@gmail.com>
2020-07-19 17:39:31 +0800

    Code cleanup

--- app/src/main/java/com/connie/noted/notepage/video/VideoFragment.kt
+++ app/src/main/java/com/connie/noted/notepage/video/VideoFragment.kt
@@ -1,18 +1,14 @@
 package com.connie.noted.notepage.video
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.connie.noted.R
-import com.connie.noted.databinding.FragmentNoteArticleBinding
 import com.connie.noted.databinding.FragmentNoteVideoBinding
 import com.connie.noted.ext.getVmFactory
 import com.connie.noted.notepage.article.ArticleFragmentArgs
-import com.connie.noted.notepage.article.ArticleViewModel
 
 class VideoFragment : Fragment() {
 
